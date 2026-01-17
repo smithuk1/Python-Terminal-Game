d16c784042c18ec64c1dce190fc7de0dba67cfd5
smithuk1 <wj.smith.uk1@gmail.com>
2026-01-17 12:17:29 +0000

    3rd commit

--- Python-Terminal-Game.py
+++ Python-Terminal-Game.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 ''' My 1st Project from Codecademy - Outside Codecademy's workspaces.
 
 Using: Python -- Git/ GitHub -- Command Line/ File Navigation.
 
 ---> A Python Terminal Game. <---
 '''
-=======
-''' My 1st Project from Codecademy - Outside Codecademy's workspaces.
-
-Using: Python -- Git/ GitHub -- Command Line/ File Navigation.
-
----> A Python Terminal Game. <---
-'''
->>>>>>> fae23e88291a6a5ccd2b65c588b8dfc8b2fe002d
